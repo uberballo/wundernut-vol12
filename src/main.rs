d73b5ae4dee8d28a74503f5ebca062c12e0ed551
kasper <kasper.paavolainen@gmail.com>
2022-09-14 18:47:46 +0300

    Format files

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 use image;
-use image::{GenericImageView, ImageBuffer,RgbImage, Rgb};
+use image::{GenericImageView, Rgb, RgbImage};
 use std::collections::HashSet;
 
-fn get_image_text(filename: String) -> Result<String, std::str::Utf8Error>{
+fn get_image_text(filename: String) -> Result<String, std::str::Utf8Error> {
     let mut lt = leptess::LepTess::new(None, "eng").unwrap();
-    lt.set_image(filename).expect("Error setting image to Leptess.");
+    lt.set_image(filename)
+        .expect("Error setting image to Leptess.");
     lt.set_source_resolution(700);
     lt.get_utf8_text()
 }
 
 fn reveal_hidden_message(filename: String, new_file: &String) -> std::io::Result<()> {
     let image = image::open(filename).expect("File not found.");
-    let (w,h) = image.dimensions();
-    let mut output = RgbImage::new(w,h);
+    let (w, h) = image.dimensions();
+    let mut output = RgbImage::new(w, h);
 
-    for (x,y, mut pixel) in image.pixels() {
+    for (x, y, mut pixel) in image.pixels() {
         if pixel[2] != 229 {
-            output.put_pixel(x,y, Rgb([0,0,0]));
+            output.put_pixel(x, y, Rgb([0, 0, 0]));
         } else {
-            output.put_pixel(x,y, Rgb([255,255,255]));
+            output.put_pixel(x, y, Rgb([255, 255, 255]));
         }
     }
 
@@ -26,32 +27,28 @@ fn reveal_hidden_message(filename: String, new_file: &String) -> std::io::Result
     Ok(())
 }
 
-
-fn check_for_hidden_messages(filename: String) -> bool{
+fn check_for_hidden_messages(filename: String) -> bool {
     let image = image::open(filename).expect("File not found.");
     let mut unique_colors = HashSet::new();
 
-    for (_,_,pixel) in image.pixels() {
+    for (_, _, pixel) in image.pixels() {
         unique_colors.insert(pixel);
     }
 
     unique_colors.len() > 1
 }
 
-fn solve_caesar_cipher(secret_text: String, rotation: u8) -> String{
+fn solve_caesar_cipher(secret_text: String, rotation: u8) -> String {
     let mut res: Vec<char> = vec![' '; secret_text.len()];
 
     for (i, character) in secret_text.chars().enumerate() {
-        res[i] = 
-        (((((character as u8) - ('A' as u8)) + (26 - rotation)) % 26) + ('A' as u8)) as char
-
+        res[i] =
+            (((((character as u8) - ('A' as u8)) + (26 - rotation)) % 26) + ('A' as u8)) as char
     }
-    res
-        .into_iter()
-        .collect()
+    res.into_iter().collect()
 }
 
-fn solve_encrypted_parchment() -> std::io::Result<Vec<String>>{
+fn solve_encrypted_parchment() -> std::io::Result<Vec<String>> {
     let filename = String::from("parchment.png");
     let new_filename = String::from("uusi.png");
 
@@ -61,9 +58,8 @@ fn solve_encrypted_parchment() -> std::io::Result<Vec<String>>{
 
     let result = match text {
         Ok(x) => x,
-        Err(error) => panic!("{}", error)
+        Err(error) => panic!("{}", error),
     };
-
 
     let ciphered: Vec<String> = result
         .split("\n")
@@ -73,11 +69,11 @@ fn solve_encrypted_parchment() -> std::io::Result<Vec<String>>{
     Ok(ciphered)
 }
 
-fn main()  {
+fn main() {
     if check_for_hidden_messages(String::from("parchment.png")) {
         match solve_encrypted_parchment() {
             Ok(result) => println!("{}", result.join(" ")),
-            Err(error) => panic!("{}", error)
+            Err(error) => panic!("{}", error),
         }
     }
 }
